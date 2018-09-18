e1098910e1f374014fb3d8aa8cb40d14ecfd1417
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-18 15:32:03 -0500

    run shotgun and get it run

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new {File.join(root, "../views/")}
 
+  get '/' do
+    erb :team
+  end
 
+  post '/teams' do
+# binding.pry
+    # @member1 = Hero.new(params[:team][members][0][:name],params[:team][members][0][:power],params[:team][members][0][:bio])
+    #
+    # @member2 = Hero.new(params[:team][members][1][:name],params[:team][members][0][:power],params[:team][members][0][:bio])
+    #
+    # @member3 = Hero.new(params[:team][members][3][:name],params[:team][members][0][:power],params[:team][members][0][:bio])
+
+    erb :super_hero
+  end
 end
